fd20edab74f624064638438e481598eccba19778
Soomin Chung <89953178+schung-21@users.noreply.github.com>
2023-03-01 20:21:21 -0600

    -

---
+++ src/components/brief.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Brief = () => {
+    return (
+        <div className="project-brief">
+            <img src='/' />
+            <div className="info">
+                <div>
+                    <h3>Overview</h3>
+                    <p></p>
+                </div>
+
+                <div>
+                    <h3>My Role</h3>
+                    <p></p>
+                </div>
+
+                <div>
+                    <h3>Material</h3>
+                    <p></p>
+                </div>
+
+                <div>
+                    <h3>Date</h3>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Brief;
